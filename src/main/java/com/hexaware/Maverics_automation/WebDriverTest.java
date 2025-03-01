8600c138c351a1373d7cec89d55d996e4aefbfb5
Yeswanth <pamulapatiy@gmail.com>
2025-03-02 01:03:30 +0530

    commmit webdrivertest

--- src/main/java/com/hexaware/Maverics_automation/WebDriverTest.java
+++ src/main/java/com/hexaware/Maverics_automation/WebDriverTest.java
@@ -1,7 +1,15 @@
 package com.hexaware.Maverics_automation;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 
 public class WebDriverTest {
 	public static WebDriver driver = null;
@@ -16,6 +24,14 @@ public class WebDriverTest {
 		System.out.println(driver.getTitle());
 		driver.navigate().refresh();
 		Thread.sleep(2000);
+		 WebElement ele = driver.findElement(By.id("windowButton"));
+		  FluentWait<WebDriver> fluentWait = new FluentWait(driver);
+		    fluentWait.withTimeout(Duration.ofSeconds(120));//Sets how long to wait for the evaluated condition to be true.
+		    fluentWait.pollingEvery(Duration.ofSeconds(20));//Sets how often the condition should be evaluated.
+		    fluentWait.ignoring(NoSuchElementException.class,StaleElementReferenceException.class);
+		    fluentWait.withMessage("Not clickable");
+		    fluentWait.until(ExpectedConditions.elementToBeClickable(ele));
+		    ele.click();
 		driver.close();
 		
 		
